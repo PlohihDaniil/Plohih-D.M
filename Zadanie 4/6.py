d0defd5ed697841d7cd63febc5147d915b2ff95e
PlohihDaniil <plohih2001@mail.ru>
2021-10-20 21:40:05 +0800

    Задание

--- Zadanie 4/6.py
+++ Zadanie 4/6.py
@@ -3,7 +3,8 @@ s=input()
 def man(s):
     i=s.count('f')
     if i >= 2:
-        print(s.find('f',+1))
+        s=s.replace('f','d',1)
+        print(s.find('f'))
     if i == 1:
         print('-1')
     if i < 1:
--- Задание 4/3.py
+++
@@ -1,5 +0,0 @@
-# -- cooding: utf-8 --
-s = input()
-def man(s):
-    print(s[(len(s) + 1) // 2:] + s[:(len(s) + 1) // 2])
-man(s)
